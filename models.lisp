1c29f5cc2080d97b2827ff04ae54555e43547e2b
fireflower0 <fireflower0@hotmail.co.jp>
2018-12-01 23:14:43 +0900

    Add insert function

--- models.lisp
+++ models.lisp
@@ -1,7 +1,8 @@
 (defpackage :cl-utopian-todo/models
   (:use :cl)
   (:import-from :mito)
-  (:export :create-todo-table))
+  (:export :create-todo-table
+           :insert-todo-table))
 (in-package :cl-utopian-todo/models)
 
 (defparameter *db-path* (asdf:system-relative-pathname :cl-utopian-todo #P"db/todo.db"))
@@ -17,3 +18,7 @@
       ((name :col-type :text)))
     (mito:execute-sql (first (mito:table-definition `tasks)))
     (mito:ensure-table-exists `tasks)))
+
+(defun insert-todo-table (data)
+  (with-connection (dbi:connect :sqlite3 :database-name *db-path*)
+    (mito:create-dao 'tasks :name data)))
